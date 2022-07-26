567a74866888dcba65a5eecbf3748f9401a0f97a
Pedro Serey <serey.pedro@gmail.com>
2022-07-26 12:36:42 -0300

    fix: formatações gerais

--- formatting.py
+++ formatting.py
@@ -2,16 +2,20 @@ def _rname(reg_add):
     return f'x{int(reg_add, 2)}'
 
 
+def calc_imm(conteudos):
+    cycle = int(conteudos['imm_cycle_32'], 2)
+    imm = int(conteudos['imm_offset'], 2)
+
+    return imm + (cycle * 32)
+
+
 def format_R(conteudos):
     inst = ''
     # exceções no funct7
-    if conteudos['funct7'] == '0100000':
-        if conteudos['opcode'] == '0010011': inst = 'srai'
-        elif conteudos['funct3'] == '000': inst = 'sub'
-        elif conteudos['funct3'] == '101': inst = 'sra'
-    elif conteudos['opcode'] == '010011':
-        if conteudos['funct3'] == '001': inst = 'slli'
-        elif conteudos['funct3'] == '101': inst = 'srli'
+    if (conteudos['funct7'] == '0100000'):
+        match conteudos['funct3']:
+            case '000': inst = 'sub'
+            case '101': inst = 'sra'
     else:
         match conteudos['funct3']:
             case '000': inst = 'add'
@@ -38,6 +42,8 @@ def format_I(conteudos):
                 case '100': inst = 'xori'
                 case '110': inst = 'ori'
                 case '111': inst = 'andi'
+
+            return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {conteudos['imm']}"
         # load?
         elif conteudos['opcode'] == '0000011':
             match conteudos['funct3']:
@@ -46,18 +52,14 @@ def format_I(conteudos):
                 case '010': inst = 'lw'
                 case '100': inst = 'lbu'
                 case '101': inst = 'lhu'
+
+            return f"{inst} {_rname(conteudos['rd'])}, {conteudos['imm']}({_rname(conteudos['rs1'])})"
     else:
         if conteudos['funct3'] == '001': inst = 'slli'
         elif conteudos['funct7'] == '0100000': inst = 'srai'
         else: inst = 'srli'
 
-    return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {conteudos['imm']}"
-
-def calc_imm(conteudos):
-    cycle = int(conteudos['imm_cycle_32'], 2)
-    imm = int(conteudos['imm_offset'], 2)
-
-    return imm + (cycle * 32)
+        return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {int(conteudos['shamt'], 2)}"
 
 
 def format_SB(conteudos):
@@ -70,7 +72,7 @@ def format_SB(conteudos):
         case '110': inst = 'bltu'
         case '111': inst = 'bgeu'
 
-    return f"{inst} {_rname(conteudos['rs2'])}, {calc_imm(conteudos)}({_rname(conteudos['rs1'])})"
+    return f"{inst} {_rname(conteudos['rs1'])}, {_rname(conteudos['rs2'])}, {calc_imm(conteudos)})"
 
 
 def format_S(conteudos):
