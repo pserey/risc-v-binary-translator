30714d1f0563fd3a32b233e65c41195cfe13a7ff
Pedro Serey <serey.pedro@gmail.com>
2022-08-02 20:17:29 -0300

    implementa formatting options

--- formatting.py
+++ formatting.py
@@ -9,14 +9,28 @@ def _rname(reg_add):
     return regs[xname]
 
 
-def calc_imm(conteudos):
+def _calc_imm(conteudos):
     cycle = int(conteudos['imm_cycle_32'], 2)
     imm = int(conteudos['imm_offset'], 2)
 
     return imm + (cycle * 32)
 
 
-def format_R(conteudos):
+def _conv_bx(value, options):
+    base = 10
+    match options:
+        case ('sb'): base = 2
+        case ('sx'): base = 16
+
+    match base:
+        case (2): return bin(value)
+        case (16): return hex(value)
+    
+    return base
+
+
+# não usa options ainda
+def format_R(conteudos, options):
     inst = ''
     stream = open('instructions.yaml', 'r')
     instrucoes = yaml.load(stream, Loader=yaml.FullLoader)
@@ -28,7 +42,7 @@ def format_R(conteudos):
     return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {_rname(conteudos['rs2'])}"
 
 
-def format_I(conteudos):
+def format_I(conteudos, options):
     inst = ''
     stream = open('instructions.yaml', 'r')
     instrucoes = yaml.load(stream, Loader=yaml.FullLoader)
@@ -39,16 +53,16 @@ def format_I(conteudos):
     # formatação para instruções de logica imediata
     if (len(conteudos) == 5):
         if conteudos['opcode'] == '0010011':
-            return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {int(conteudos['imm'], 2)}"
+            return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {_conv_bx(int(conteudos['imm'], 2), options)}"
         # formatação para instruções de load
         elif conteudos['opcode'] == '0000011':
             return f"{inst} {_rname(conteudos['rd'])}, {conteudos['imm']}({_rname(conteudos['rs1'])})"
     # formatação para instruções de shift
     else:
-        return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {int(conteudos['shamt'], 2)}"
+        return f"{inst} {_rname(conteudos['rd'])}, {_rname(conteudos['rs1'])}, {_conv_bx(int(conteudos['shamt'], 2), options)}"
 
 
-def format_SB(conteudos):
+def format_SB(conteudos, options):
     inst = ''
     stream = open('instructions.yaml', 'r')
     instrucoes = yaml.load(stream, Loader=yaml.FullLoader)
@@ -56,10 +70,10 @@ def format_SB(conteudos):
     for dict in instrucoes['SB']:
         if conteudos['funct3'] == dict['funct3']: inst = dict['name']
 
-    return f"{inst} {_rname(conteudos['rs1'])}, {_rname(conteudos['rs2'])}, {calc_imm(conteudos)}"
+    return f"{inst} {_rname(conteudos['rs1'])}, {_rname(conteudos['rs2'])}, {_conv_bx(_calc_imm(conteudos), options)}"
 
 
-def format_S(conteudos):
+def format_S(conteudos, options):
     inst = ''
     stream = open('instructions.yaml', 'r')
     instrucoes = yaml.load(stream, Loader=yaml.FullLoader)
@@ -67,4 +81,4 @@ def format_S(conteudos):
     for dict in instrucoes['S']:
         if conteudos['funct3'] == dict['funct3']: inst = dict['name']
 
-    return f"{inst} {_rname(conteudos['rs2'])}, {calc_imm(conteudos)}({_rname(conteudos['rs1'])})"
+    return f"{inst} {_rname(conteudos['rs2'])}, {_conv_bx(_calc_imm(conteudos), options)}({_rname(conteudos['rs1'])})"
