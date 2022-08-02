b6a1990b2fa5dd0e335f185e7ab7e252de07bc2e
Pedro Serey <serey.pedro@gmail.com>
2022-08-02 20:18:51 -0300

    fix: opções em menu

--- conv.py
+++ conv.py
@@ -24,17 +24,17 @@ def inst_decode(instruction, options):
             conteudos = {}
             conteudos['funct7'], conteudos['rs2'], conteudos['rs1'], conteudos['funct3'], conteudos['rd'], conteudos['opcode'] = decode_R(instruction)
 
-            return format_R(conteudos)
+            return format_R(conteudos, options)
         case 'SB':
             conteudos = {}
             conteudos['imm_cycle_32'], conteudos['rs2'], conteudos['rs1'], conteudos['funct3'], conteudos['imm_offset'], conteudos['opcode'] = decode_SB(instruction)
 
-            return format_SB(conteudos)
+            return format_SB(conteudos, options)
         case 'S':
             conteudos = {}
             conteudos['imm_cycle_32'], conteudos['rs2'], conteudos['rs1'], conteudos['funct3'], conteudos['imm_offset'], conteudos['opcode'] = decode_S(instruction)
 
-            return format_S(conteudos)
+            return format_S(conteudos, options)
         case 'I':
             imm, rs1, funct3, rd, opcode = decode_I(instruction)
             conteudos = {}
@@ -56,7 +56,7 @@ def inst_decode(instruction, options):
                 conteudos['rd'] = rd
                 conteudos['opcode'] = opcode
 
-            return format_I(conteudos)
+            return format_I(conteudos, options)
         case 'U':
             return 'funções U e UJ não implementadas'
         case 'UJ':
@@ -66,27 +66,24 @@ def inst_decode(instruction, options):
 def main():
     print('as instruções serão em binário ou hexadecimal? (b/x)')
     r = input()
+    print('opções (sd/sx): ')
+    op = input()
     match r:
         case 'b':
             while True:
                 print('instrução (para sair, digite s): ')
                 inst = input()
                 if (inst == 's'): break
-                print('opções (sd/sx): ')
-                op = input()
 
-                print(inst_decode(f'{inst:0>32}'))
+                print(inst_decode(f'{inst:0>32}', op))
         case 'x':
             while True:
                 print('instrução (para sair, digite s): ')
                 inst = f'{input()}'
                 if (inst == 's'): break
-                print('opções (sd/sx): ')
-                op = input()
 
                 hex_to_bin = f'{bin(int(inst, 16))[2:]:0>32}'
-                instrucao = inst_decode(hex_to_bin)
-                print(instrucao)
+                print(inst_decode(hex_to_bin, op))
 
 
 if __name__ == '__main__':
